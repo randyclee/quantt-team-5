516ee08ca621d925e2c19fe7847d68f30d8bc145
randyclee <randy.c.lee2@gmail.com>
2021-12-30 18:01:22 -0500

    restored management file

---
+++ Management.py
@@ -0,0 +1,107 @@
+import time
+import datetime
+import pandas as pd
+import numpy as np
+import technical_analysis as ta
+import chart_analysis as ca
+import sentiment_analysis as sa
+
+class Management(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2021, 6, 5)
+        
+        self.cash = 100000000.00
+        self.mas_ta, self.mas_ca, self.mas_sa = [], [], []
+        self.dailys_ta, self.dailys_ca, self.dailys_sa = [], [], []
+        self.currents = [self.cash, 0.00, 0.00, 0.00]
+        
+        self.ALLOCATIONS = [
+            [0.0001, 0.05, 0.50],
+            [0.15, 0.35, 0.65],
+            [0.05, 0.25, 0.55],
+            [0.00, 0.15, 0.30]
+        ]
+        
+        self.ta.initialize(self.cash*self.ALLOCATIONS[1][1])
+        self.ca.initialize(self.cash*self.ALLOCATIONS[2][1])
+        self.sa.initialize(self.cash*self.ALLOCATIONS[3][1])
+        
+        main()
+    
+    def calc_ma(dailys):
+        period = 5
+        data = np.array(dailys)
+        return (np.convolve(dailys, np.ones(period), 'valid') / period)
+    
+    def check_bounds():
+        total_value = sum(currents)
+        changes = [0.00, 0.00, 0.00, 0.00]
+    
+        for i in range(1,4):
+            if(currents[i] < total_value * self.ALLOCATIONS[i][0]):
+                change = round((total_value * self.ALLOCATIONS[i][0]) - currents[i], 2)
+                if(check_cash(total_value, change)):
+                    changes[i-1] = change
+                    self.cash = (sum(changes) * -1)
+    
+                else:
+                    changes[i-1] = self.cash - (total_value * self.ALLOCATIONS[0][0])
+                    self.cash = (sum(changes) * -1)
+    
+            elif(currents[i] > total_value * self.ALLOCATIONS[i][2]):
+                change = round((total_value * self.ALLOCATIONS[i][0]) - currents[i], 2)
+    
+        return changes
+    
+    def reallocate():
+        total_value = sum(currents)
+        changes = [0.00, 0.00, 0.00]
+    
+        for i in range(1,4):
+            change = round((total_value * self.ALLOCATIONS[i][0]) - currents[i], 2)
+            changes[i-1] = change
+            self.cash = sum(changes) * -1
+            return changes
+        
+        return changes
+    
+    def check_cash(total_value, change):
+        proposed = self.cash + (change * -1)
+        return (proposed > total_value * self.ALLOCATIONS[0][0])
+    
+    def main():
+        print("started")
+        while True:
+            self.current_ta = self.ta.get_value()
+            self.dailys_ta.append(self.current_ta)
+            #mas_ta.append(calc_ma(dailys_ta))
+    
+            self.current_ca = self.ca.get_value()
+            self.dailys_ta.append(self.current_ca)
+            #mas_ta.append(calc_ma(dailys_ta))
+    
+            self.current_sa = self.sa.get_value()
+            self.dailys_ta.append(self.current_sa)
+            #mas_ta.append(calc_ma(dailys_ta))
+    
+            if(datetime.datetime.today().weekday() == 0):
+                reallocations = reallocate()
+                self.cash = self.cash + (reallocations[0] * -1)
+                self.ta.reallocate(reallocations[1])
+                self.sa.reallocate(reallocations[2])
+                self.ca.reallocate(reallocations[3])
+                
+            else if(datetime.datetime.today().weekday() == 5 or datetime.datetime.today().weekday() == 6):
+                pass
+                
+    
+            else:
+                reallocations = check_bounds()
+                self.cash = self.cash + (reallocations[0] * -1)
+                self.ta.reallocate(reallocations[1])
+                self.sa.reallocate(reallocations[2])
+                self.ca.reallocate(reallocations[3])
+    
+                
+            time.sleep(86400)
---
+++ TA_multupleStocks.py
@@ -0,0 +1,65 @@
+from AlgorithmImports import *
+import pandas as pd
+
+
+class MACDTrendAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+        '''Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.'''
+
+        self.SetStartDate(2019, 1, 1)    #Set Start Date
+        self.SetCash(100000)             #Set Strategy Cash
+
+        self.data = {}
+        self.symbols = [
+            'AAPL','MSFT','AMZN','FB','BRK.B','GOOGL','GOOG','JPM','JNJ','V','PG','XOM','UNH','BAC','MA','T','DIS','INTC','HD','VZ','MRK','PFE',
+            'CVX','KO','CMCSA','CSCO','PEP','WFC','C','BA','ADBE','WMT','CRM','MCD','MDT','BMY','ABT','NVDA','NFLX','AMGN','PM','PYPL','TMO',
+            'COST','ABBV','ACN','HON','NKE','UNP','UTX','NEE','IBM','TXN','AVGO','LLY','ORCL','LIN','SBUX','AMT','LMT','GE','MMM','DHR','QCOM',
+            'CVS','MO','LOW','FIS','AXP','BKNG','UPS','GILD','CHTR','CAT','MDLZ','GS','USB','CI','ANTM','BDX','TJX','ADP','TFC','CME','SPGI',
+            'COP','INTU','ISRG','CB','SO','D','FISV','PNC','DUK','SYK','ZTS','MS','RTN','AGN','BLK'
+            ]
+
+        for symbol in self.symbols:
+            self.AddEquity(symbol,Resolution.Daily)
+            self.data[symbol] = self.MACD(symbol, 12, 26, 9, MovingAverageType.Exponential, Resolution.Daily)
+
+        # define our daily macd(12,26) with a 9 day signal
+        self.__previous = datetime.min
+
+
+    def OnData(self, data):
+
+        if self.IsWarmingUp: return
+
+        # only once per day
+        if self.__previous.date() == self.Time.date(): return
+
+        # define a small tolerance on our checks to avoid bouncing
+        tolerance = 0.0025
+        chooseStock = np.zeros(len(self.symbols))
+        counter = 0
+        # if our macd is greater than our signal, go long
+        for symbol in self.symbols:
+            if self.data[symbol].IsReady:
+
+                holdings = self.Portfolio[symbol].Quantity
+                signalDeltaPercent = (self.data[symbol].Current.Value - self.data[symbol].Signal.Current.Value)/self.data[symbol].Fast.Current.Value
+
+                if holdings <= 0 and signalDeltaPercent > tolerance:  # 0.01%
+                    chooseStock[counter] = 1
+                # of our macd is less than our signal, then go short
+                elif holdings >= 0 and signalDeltaPercent < -tolerance:
+                    chooseStock[counter] = 2
+
+            counter = counter + 1
+
+        buyPercent = 1/len(self.symbols)
+        counter = 0
+        for symbol in self.symbols:
+            if(chooseStock[counter] == 1):
+                self.SetHoldings(symbol, buyPercent)
+            elif(chooseStock[counter] == 2):
+                self.Liquidate(symbol)
+            counter = counter + 1
+
+        self.__previous = self.Time
